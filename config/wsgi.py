3336ba4371e3259eed47842b8e576a37689cc620
fujiwara yasuyo <y.fujiwara.sys24@morijyobi.ac.jp>
2025-08-07 15:45:54 +0900

    wsgi.pyの修正

--- config/wsgi.py
+++ config/wsgi.py
@@ -6,12 +6,14 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
+import os
+
+from django.core.wsgi import get_wsgi_application
+import django # 追加
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-import os
-from django.core.wsgi import get_wsgi_application
-import django # 追加
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
